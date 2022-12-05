1b10284b73a5d2a841c3e4d3aeaaf7fbf8d41787
kdreamix <wwwyingkit@gmail.com>
2022-12-05 17:58:56 +0800

    beautify

--- app/src/main/java/com/kitmak/composewave/MainActivity.kt
+++ app/src/main/java/com/kitmak/composewave/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.kitmak.composewave
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.withFrameMillis
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.input.pointer.pointerInput
 import com.kitmak.composewave.ui.theme.ComposeWaveTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Box(contentAlignment = Alignment.Center) {
+                    LaunchedEffect(Unit) {
+                        while (true) {
+                            withFrameMillis {
+                                WaterWave.doWave()
+                            }
+                        }
+                    }
+
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    //     .pointerInput(Unit) {
+                    //         detectTapGestures {
+                    //         Log.d("WATER", "click $it")
+                    //         WaterWave.onClick(Offset(25f, 25f))
+                    //     }
+                    // }.drawBehind {
+                    //         WaterWave.invalidator.apply {
+                    //             drawImage(WaterWave.imageBitmap.asImageBitmap())
+                    //         }
+                    // }
+                    ) {
                         WaterRipple()
                     }
                 }
--- app/src/main/java/com/kitmak/composewave/Ripples.kt
+++ app/src/main/java/com/kitmak/composewave/Ripples.kt
@@ -4,77 +4,48 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.foundation.Canvas
-import androidx.compose.ui.graphics.Canvas as Canvas2
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.graphics.drawscope.translate
-import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.input.pointer.pointerInput
-import com.kitmak.composewave.WaterWave.cols
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+import com.kitmak.composewave.WaterWave.COLS
+import com.kitmak.composewave.WaterWave.imageBitmap
+import com.kitmak.composewave.WaterWave.invalidator
 import com.kitmak.composewave.WaterWave.onClick
-import com.kitmak.composewave.WaterWave.rows
-import kotlinx.coroutines.delay
 import kotlin.random.Random
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun WaterRipple(modifier: Modifier = Modifier) {
 
-
     LaunchedEffect(Unit) {
-
         while (true) {
-            withFrameNanos {
+            withFrameMillis {
                 WaterWave.doWave()
             }
         }
-
     }
+    val size = with(LocalDensity.current) { (COLS * 5).toDp() }
+
     Canvas(modifier = Modifier
         .background(Color.Black)
-        .fillMaxSize()
+        .size(size)
         .pointerInput(Unit) {
             detectTapGestures {
                 Log.d("WATER", "click $it")
-                onClick(Offset(25f, 25f))
+                onClick(it)
             }
         }) {
 
-//        for (i in 1 until cols * cols){
-////            val index = WaterWave.getIndex(i,j)
-//            val color = WaterWave.curr[i].toInt() * 255
-//            drawCircle(
-//                Color(
-//                    color,
-//                    color,color
-//                ), center = getXY(i), radius = 2f
-//            )
-//        }
-        // Manually create a Bitmap
-
-//        val canvasBitmap = Canvas2(imageBitmap)
-
-
-//        for (i in 1 until cols) {
-//            for (j in 1 until rows) {
-//
-//                val index = WaterWave.getIndex(i, j)
-//                val color = WaterWave.curr[index].toInt() * 255
-//                val i  = i*10
-//                val j = j* 10
-//                imageBitmap?.setPixel(i,j, android.graphics.Color.rgb(color,color,color))
-//
-//            }
-//        }
-        WaterWave.imageBitmap?.let {
-            this.drawImage(it.asImageBitmap())
+        invalidator.apply {
+            drawImage(imageBitmap.asImageBitmap())
         }
 
     }
@@ -82,60 +53,61 @@ fun WaterRipple(modifier: Modifier = Modifier) {
 
 
 object WaterWave {
-    const val damping = 0.9f
-    val cols = 50
-    val rows = 50
+    private const val DAMPENING = 0.98f
+    private const val N = 250
+    const val COLS = N
+    private const val ROWS = N
 
-    var curr = mutableStateListOf<Float>()
-    var prev = mutableStateListOf<Float>()
-    var imageBitmap: Bitmap? by mutableStateOf(null)
+    var invalidator by mutableStateOf(0)
+    var curr = mutableListOf<Float>()
+    var prev = mutableListOf<Float>()
 
-
+    var imageBitmap: Bitmap by mutableStateOf(Bitmap.createBitmap(
+        COLS * 5,
+        ROWS * 5,
+        Bitmap.Config.ARGB_8888
+    ))
 
     init {
-        curr.addAll(MutableList(rows * cols) { 0f })
-        prev.addAll(MutableList(rows * cols) { 0f })
+        curr.addAll(MutableList(ROWS * COLS) { 0f })
+        prev.addAll(MutableList(ROWS * COLS) { 0f })
     }
 
     fun onClick(offset: Offset) {
-//        val x = Random.nextInt(1, cols)
-//        val y = Random.nextInt(1, cols)
+       // val x = Random.nextInt(1, COLS)
+       // val y = Random.nextInt(1, COLS)
 //        Log.d("WATER", "xy:$x, $y")
-        val clickIndex = getIndex(offset.x.toInt(), offset.y.toInt())
+        val clickIndex = getIndex(offset.x.toInt()/5, offset.y.toInt()/5)
         curr[clickIndex] = 255f
     }
 
     fun getIndex(x: Int, y: Int): Int {
-        return x + y * cols
+        return x + y * COLS
     }
 
     fun doWave() {
-
-        for (i in rows until cols * cols - rows) {
-            curr[i] = (prev[i + 1] + prev[i - 1] + prev[i + cols] + prev[i - cols]) / 2 - curr[i]
-            curr[i] = curr[i] * damping
+        for (i in ROWS until COLS * COLS - ROWS) {
+            curr[i] = (prev[i + 1] + prev[i - 1] + prev[i + COLS] + prev[i - COLS]) / 2 - curr[i]
+            curr[i] = curr[i] * DAMPENING
         }
+
         //swap
         val tmp = prev
         prev = curr
         curr = tmp
 
-        imageBitmap = Bitmap.createBitmap(
-            cols*10,
-            rows*10,
-            Bitmap.Config.ARGB_8888
-        )
-        for (i in 1 until cols) {
-            for (j in 1 until rows) {
-
-                val index = WaterWave.getIndex(i, j)
-                val color = WaterWave.curr[index].toInt() * 255
-                val i  = i*10
-                val j = j* 10
-                imageBitmap?.setPixel(i,j, android.graphics.Color.rgb(color,color,color))
+        // rendering
+        for (i in 1 until COLS) {
+            for (j in 1 until ROWS) {
+                val index = getIndex(i, j)
+                val color = curr[index].toInt()
+                val i = i * 5
+                val j = j * 5
+                imageBitmap.setPixel(i,j, android.graphics.Color.rgb(color,color,color))
 
             }
         }
+        invalidator ++
     }
 }
 
