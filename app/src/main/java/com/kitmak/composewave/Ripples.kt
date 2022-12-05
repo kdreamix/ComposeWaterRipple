94bbba320865a2303eac509d183e9dd18b135fa6
kdreamix <wwwyingkit@gmail.com>
2022-12-06 00:05:28 +0800

    Upda

--- app/src/main/java/com/kitmak/composewave/Ripples.kt
+++ app/src/main/java/com/kitmak/composewave/Ripples.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
@@ -25,99 +26,110 @@ import kotlin.random.Random
 @Composable
 fun WaterRipple(modifier: Modifier = Modifier) {
 
-  LaunchedEffect(Unit) {
-    while (true) {
-      withFrameMillis {
-        WaterWave.doWave()
-      }
+    LaunchedEffect(Unit) {
+        while (true) {
+            withFrameMillis {
+                WaterWave.doWave()
+            }
+        }
     }
-  }
-  val size = with(LocalDensity.current) { (COLS * WaterWave.SCALE).toDp() }
+    val size = with(LocalDensity.current) { (COLS * WaterWave.SCALE).toDp() }
 
-  Canvas(modifier = Modifier
-    .background(Color.Black)
-    .size(size)
-    .pointerInput(Unit) {
-      detectTapGestures {
-        Log.d("WATER", "click $it")
-        onClick(it)
-      }
-    }) {
+    Canvas(modifier = Modifier
+        .background(Color(0, 54, 119))
+        .size(size)
+        .pointerInput(Unit) {
+            detectTapGestures {
+                onClick(it)
+            }
+        }
+        .pointerInput(Unit){
+            detectDragGestures(
+                onDragStart = {
+                    onClick(it)
+                },
+                onDrag = { change, _ ->
+                    onClick(change.position)
+                }
+            )
 
-    invalidator.apply {
-      drawImage(imageBitmap.asImageBitmap())
+        }) {
+
+        invalidator.apply {
+            drawImage(imageBitmap.asImageBitmap())
+        }
+
     }
-
-  }
 }
 
 
 object WaterWave {
-  const val DAMPENING = 0.95f
-  const val AMPLITUDE = 3
-  const val SCALE = 5
-  const val N = 250
-  const val COLS = N
-  const val ROWS = N
+    const val DAMPENING = 64
+    const val AMPLITUDE = 3
+    const val SCALE = 5
+    const val N = 250
+    const val COLS = N
+    const val ROWS = N
+    const val MAX = ROWS * COLS
 
-  var invalidator by mutableStateOf(0)
-  var curr = mutableListOf<Float>()
-  var prev = mutableListOf<Float>()
+    var invalidator by mutableStateOf(0)
+    var curr = mutableListOf<Float>()
+    var prev = mutableListOf<Float>()
 
-  var imageBitmap: Bitmap by mutableStateOf(Bitmap.createBitmap(
-    COLS * SCALE,
-    ROWS * SCALE,
-    Bitmap.Config.ARGB_8888
-  ))
+    var imageBitmap: Bitmap by mutableStateOf(
+        Bitmap.createBitmap(
+            COLS * SCALE,
+            ROWS * SCALE,
+            Bitmap.Config.ARGB_8888
+        )
+    )
 
-  init {
-    curr.addAll(MutableList(ROWS * COLS) { 0f })
-    prev.addAll(MutableList(ROWS * COLS) { 0f })
-  }
+    init {
+        curr.addAll(MutableList(ROWS * COLS) { 0f })
+        prev.addAll(MutableList(ROWS * COLS) { 0f })
+    }
 
-  fun onClick(offset: Offset) {
-    // val x = Random.nextInt(1, COLS)
-    // val y = Random.nextInt(1, COLS)
+    fun onClick(offset: Offset) {
+        if (offset.x < 0 || offset.y < 0) return
+        // val x = Random.nextInt(1, COLS)
+        // val y = Random.nextInt(1, COLS)
 //        Log.d("WATER", "xy:$x, $y")
-    val clickIndex = getIndex(offset.x.toInt() / SCALE, offset.y.toInt() / SCALE)
-    curr[clickIndex] = 255f
-  }
+        val clickIndex = getIndex(offset.x.toInt() / SCALE, offset.y.toInt() / SCALE)
+        if (clickIndex > MAX || clickIndex < 0) return
 
-  fun getIndex(x: Int, y: Int): Int {
-    return x + y * COLS
-  }
-
-  fun doWave() {
-    for (i in ROWS until COLS * COLS - ROWS) {
-      curr[i] =
-        (prev[i + 1] + prev[i - 1] + prev[i + COLS] + prev[i - COLS])/2f - curr[i]
-      curr[i] = curr[i] * DAMPENING
-      // curr[i] -= curr[i].toInt().shr(5).toFloat()
+        curr[clickIndex] = 255f
     }
-    //swap
-    prev = curr.apply { curr = prev }
 
-
-    // rendering
-    for (i in 1 until COLS) {
-      for (j in 1 until ROWS) {
-        val index = getIndex(i, j)
-        val color = curr[index].toInt()
-        imageBitmap.drawPixels(i* SCALE, j* SCALE, android.graphics.Color.rgb(color, color, color))
-
-      }
+    fun getIndex(x: Int, y: Int): Int {
+        return x + y * COLS
     }
-    invalidator++
-  }
 
-  inline fun Bitmap.drawPixels(i: Int, j: Int, brightness: Int) {
-    // setPixels(IntArray(4){brightness}, getIndex(i, j), COLS * SCALE, i,j,2,2)
-    setPixel(i, j, android.graphics.Color.rgb(brightness, brightness, brightness))
-    // setPixel(i + AMPLITUDE, j, android.graphics.Color.rgb(brightness, brightness, brightness))
-    // setPixel(i - AMPLITUDE, j, android.graphics.Color.rgb(brightness, brightness, brightness))
-    // setPixel(i, j + AMPLITUDE, android.graphics.Color.rgb(brightness, brightness, brightness))
-    // setPixel(i, j - AMPLITUDE, android.graphics.Color.rgb(brightness, brightness, brightness))
-  }
+    fun doWave() {
+        for (i in ROWS until COLS * COLS - ROWS) {
+            curr[i] = (prev[i + 1] + prev[i - 1] + prev[i + COLS] + prev[i - COLS]) / 2f - curr[i]
+            // Dampening
+            curr[i] = curr[i] - curr[i] / DAMPENING
+        }
+        //swap
+        prev = curr.apply { curr = prev }
+
+
+        // rendering
+        for (i in 1 until COLS) {
+            for (j in 1 until ROWS) {
+                val index = getIndex(i, j)
+                val brightness = curr[index].toInt()
+                imageBitmap.setPixel(
+                    i * SCALE,
+                    j * SCALE,
+                    android.graphics.Color.rgb(brightness, brightness, brightness)
+                )
+
+
+            }
+        }
+        invalidator++
+    }
 }
 
 
